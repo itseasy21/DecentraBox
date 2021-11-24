be0f6d9b0648eacb17d63889f8c546fcaaf40641
Shubham Mathur <itseasy21@gmail.com>
2021-11-24 19:47:39 +1100

    Fixes for header & landing page url

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,22 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Dashboard from './pages/Dashboard';
+import Landing from './pages/Landing';
 import ProcessLogin from './pages/ProcessLogin';
 import Settings from './pages/Settings';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import theme from './theme/theme';
-import Landing from './pages/Landing';
 
 const AppRoutes: React.FC = () => {
     return (
         <Switch>
             <Route path="/">
-                <Route exact path="/" component={SignIn} />
+                <Route exact path="/signin" component={SignIn} />
                 <Route exact path="/signup" component={SignUp} />
                 <Route exact path="/dashboard" component={Dashboard} />
                 <Route exact path="/settings" component={Settings} />
-                <Route exact path="/landing" component={Landing} />
+                <Route exact path="/" component={Landing} />
                 <Route exact path="/processLogin/:email" component={ProcessLogin} />
                 {/* <Route path="/trending" component={Trending} />
                 <Route path="/global" component={GlobalMarket} /> */}
